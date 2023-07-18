8a0fb98a15b8625fed9165ce47c8d667e3996235
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-07-19 01:03:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,3 @@ function openSidebar() {
 function closeSideBar() {
   document.getElementById("sidebar").style.width = "0";
 }
-
-document.addEventListener("visibilitychange", function() {
-
-      if (document.hidden) {
-	  
-		document.title = "(⌐■_■)";
-		
-		setTimeout(function() {
-			document.title = "( •_•)>⌐■-■";
-        }, 500);
-		
-		setTimeout(function() {
-			document.title = "Aqwam's Portfolio";
-        }, 1000);
-		
-      } else {
-	  
-        document.title = "( •_•)>⌐■-■";
-		
-		setTimeout(function() {
-			document.title = "(⌐■_■)";
-        }, 500); // 1000 milliseconds = 1 second
-		
-      }
-	  
-});
